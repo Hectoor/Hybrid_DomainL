89447ae2c01f73f66dcfa29204a69e29f841b761
Hectoor <418918345@qq.com>
2022-05-23 22:57:53 +0800

    update

--- scripts/evaluate.sh
+++
@@ -1,7 +0,0 @@
-set -ex
-ls
-export PYTHONPATH=`pwd`
-
-
-CUDA_VISIBLE_DEVICES=1 python evaluate.py \
-        --exp-root='/mnt/mdisk/zyh/asanet/asanethdl/hdl/'
--- scripts/train_gta2city.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-set -ex
-ls
-export PYTHONPATH=`pwd`
-
-CUDA_VISIBLE_DEVICES=3 python ../HDL_trainG2C.py --snapshot-dir /media/HDD_4TB/zyh/HDL/g2c_hds --restore-from /media/HDD_4TB/zyh/GTA5_init.pth --num-steps 200000 --num-steps-stop 200000
